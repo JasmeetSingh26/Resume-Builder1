961d2e471a1782ce2d17b9f2b94371b9bb8d46f3
Jasmeet Singh <singhjasmeet200526@gmail.com>
2025-01-21 18:53:01 +0530

    next

--- src/components/resume-ui/Extra.jsx
+++ src/components/resume-ui/Extra.jsx
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
-import { ResumeContext } from '@/contexts/ResumeContext';
-import Description from './Description';
-import Title from './Title';
+import { useContext } from "react";
+import { ResumeContext } from "@/contexts/ResumeContext";
+import Description from "./Description";
+import Title from "./Title";
 
 const Extra = () => {
   const { extra, toggle } = useContext(ResumeContext);
@@ -10,8 +10,8 @@ const Extra = () => {
     <>
       {toggle.extraToggle && (
         <div>
-          <Title title='Achievements and Certifications' />
-          <div className='pl-0 md:pl-4 mt-2'>
+          <Title title="Achievements" />
+          <div className="pl-0 md:pl-4 mt-2">
             {extra && <Description des={extra} />}
           </div>
         </div>

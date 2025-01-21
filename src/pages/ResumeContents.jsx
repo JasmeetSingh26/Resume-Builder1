14e96732ef73d472484293e2691bfa4b168167a1
Jasmeet Singh <singhjasmeet200526@gmail.com>
2025-01-21 18:43:30 +0530

    next

--- src/pages/ResumeContents.jsx
+++ src/pages/ResumeContents.jsx
@@ -1,18 +1,18 @@
-import PersonalDetails from '@/components/resume-ui/PersonalDetails';
-import Education from '@/components/resume-ui/Education';
-import Project from '@/components/resume-ui/Project';
-import Experience from '@/components/resume-ui/Experience';
-import Skills from '@/components/resume-ui/Skills';
-import Extra from '@/components/resume-ui/Extra';
+import PersonalDetails from "@/components/resume-ui/PersonalDetails";
+import Education from "@/components/resume-ui/Education";
+import Project from "@/components/resume-ui/Project";
+import Experience from "@/components/resume-ui/Experience";
+import Skills from "@/components/resume-ui/Skills";
+import Extra from "@/components/resume-ui/Extra";
 
 const ResumeContents = () => {
   return (
-    <div className='bg-white px-[40px] py-[30px] min-h-full font-noto'>
+    <div className="bg-white px-[40px] py-[30px] min-h-full font-noto">
       <PersonalDetails />
-      <Education />
+      <Skills />
       <Experience />
       <Project />
-      <Skills />
+      <Education />
       <Extra />
     </div>
   );
